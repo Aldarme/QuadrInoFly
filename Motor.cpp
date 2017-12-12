4b5e6fe3b4b4679b4f65935901d3a6c2f66e921f
Unknown <rometpierre@gmail.com>
2017-12-12 23:30:13 +0100

    Add file for motor control

---
+++ Motor.cpp
@@ -0,0 +1,68 @@
+
+
+#include "Define.h"
+/******************Preprocessor****************/
+
+
+/*******************Variable******************/
+Servo ESC1;
+Servo ESC2;
+Servo ESC3;
+Servo ESC4;
+
+
+
+/*******************Function******************/
+/*
+*   Init & Configure ESC
+*   Use pwm Pin 5,6,9,10
+*/
+void configMotor()
+{
+    ESC1.attach(5);
+    ESC2.attach(6);
+    ESC3.attach(10);
+    ESC4.attach(9);
+
+    ESC1.writeMicroseconds(1000);
+    ESC2.writeMicroseconds(1000);
+    ESC3.writeMicroseconds(1000);
+    ESC4.writeMicroseconds(1000);
+
+    //After setting initialisation, ESC need a delay before setting speed for motor
+    delay(2000);
+}
+
+/*
+*   Allow to set motor's speed
+*/
+void setSpeedGlobal(int pPpm)
+{
+    ESC1.writeMicroseconds(pPpm);
+	ESC2.writeMicroseconds(pPpm);
+	ESC3.writeMicroseconds(pPpm);
+	ESC4.writeMicroseconds(pPpm);
+}
+
+void setSpeedA(int pPpm)
+{
+    ESC1.writeMicroseconds(pPpm);
+}
+
+void setSpeedB(int pPpm)
+{
+	ESC2.writeMicroseconds(pPpm);
+}
+
+/*
+*   Allow to set motor's speed
+*/
+void setSpeedC(int pPpm)
+{
+	ESC3.writeMicroseconds(pPpm);
+}
+
+void setSpeedD(int pPpm)
+{
+	ESC4.writeMicroseconds(pPpm);
+}
---
+++ Motor.h
@@ -0,0 +1,11 @@
+#ifndef MOTOR_H_INCLUDED
+#define MOTOR_H_INCLUDED
+
+void configMotor();
+void setSpeedGlobal(int pPpm);
+void setSpeedA(int pPpm);
+void setSpeedB(int pPpm);
+void setSpeedC(int pPpm);
+void setSpeedD(int pPpm);
+
+#endif // MOTOR_H_INCLUDED
